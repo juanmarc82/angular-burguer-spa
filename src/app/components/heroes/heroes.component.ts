abc2479876785deb5c8927fed46d961c21919f77
juanmarc82 <juanmarojascardenas@gmail.com>
2020-03-21 18:32:48 +0100

    Add Heroes

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,14 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { HeroesService } from '$services/heroes.service'
 
 @Component({
   selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
+  templateUrl: './heroes.component.html'
 })
 export class HeroesComponent implements OnInit {
+  heroes: Heroe[] = []
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private _heroesService: HeroesService) {
   }
 
+  ngOnInit() {
+    this.heroes = this._heroesService.getHeroes()
+  }
+}
+
+interface Heroe {
+  nombre: string
+  bio: string
+  img: string
+  aparicion: string
+  casa: string
 }
